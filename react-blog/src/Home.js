94adec0f73ae2af483db236a141198f5027c8aff
Arya Krishna <dr.aryakris@outlook.com>
2022-05-30 12:03:28 -0700

    useHooks

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
 //    let name = 'mario';
 const[name, setName] = useState('mario');
+const [age, setAge] = useState(25);
    const handleClick = () =>{
        setName('luigi')
    }
@@ -13,7 +14,7 @@ const[name, setName] = useState('mario');
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick = {handleClick}> Click me</button>
             
         </div>
