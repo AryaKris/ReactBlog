88216751b401cdeb3aae88361d628fa756fb7116
Arya Krishna <dr.aryakris@outlook.com>
2022-05-30 18:58:09 -0700

    using props to make components reusable

---
+++ react-blog/src/BlogList.js
@@ -0,0 +1,24 @@
+//props are ways in which we can pass data from a parent component to child component. Here our parent component is home.js amd bloglist is our child component. Props can make components more reusable.
+
+const BlogList = ({ blogs, title }) => {
+    // const blogs  = props.blogs;
+    // console.log(props, blogs);
+
+    return ( 
+
+        <div className = "blog-list">
+            <h2>{title}</h2>
+
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> {blog.title}</h2>
+                    <p> Written by {blog.author}</p>
+
+
+                </div>
+            ))}  
+        </div>
+     );
+}
+ 
+export default BlogList;
